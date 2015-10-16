f5e18576f49a236655619dbb571e6e1c359aa0fb
Isaac Murchie <isaac@saucelabs.com>
2015-10-16 16:25:52 -0700

    Add travis config

--- test/unit/utils-specs.js
+++ test/unit/utils-specs.js
@@ -53,6 +53,7 @@ describe('util', () => {
       execStub.calledWith('pkill', ['-9', '-f', 'iOS Simulator']).should.be.true;
     });
     it('should ignore errors thrown by exec', async () => {
+      xcodeMock.expects('getVersion').withArgs(true).returns(Promise.resolve(XCODE_VERSION_7));
       execStub.throws();
       await killAllSimulators().should.not.be.rejected;
       execStub.threw().should.be.true;
