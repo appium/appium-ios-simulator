f2270549024096cf587ee35ad3f53e7c566d25b7
Dan Graham <dan@saucelabs.com>
2016-11-18 18:09:28 -0800

    Fixed redundancies in cert specs
    Made use of before and beforeEach to reduce boilerplate

--- test/unit/certificate-specs.js
+++ test/unit/certificate-specs.js
@@ -14,18 +14,18 @@ let cwd = process.cwd();
 let assetsDir = `${cwd}/test/assets`;
 let keychainsDir = `${assetsDir}/Library/Keychains`;
 let keychainsDirOriginal;
+let certificate;
+let trustStore;
+let testUUID;
 
-describe('when using Certificate class', () => { 
+describe('when using TrustStore class', () => {
 
   beforeEach(() => {
-    keychainsDirOriginal = `${assetsDir}/Library/Keychains-Original`;
-    fse.emptyDirSync(keychainsDir);
-    fse.copySync(keychainsDirOriginal, keychainsDir); 
+    trustStore = new TrustStore(assetsDir);
+    testUUID = uuid.v4();
   });
 
   it('can add a record to the TrustStore tsettings', async () => {
-    let trustStore = new TrustStore(assetsDir);
-    let testUUID = uuid.v4();
     let tsettings = await trustStore.getRecords(testUUID); 
     chai.assert(tsettings.length===0);
     await trustStore.addRecord(uuid.v4(), 'tset', testUUID, 'data');
@@ -35,8 +35,6 @@ describe('when using Certificate class', () => {
   });
 
   it('can add and remove records to in TrustStore tsettings', async () => {
-    let trustStore = new TrustStore(assetsDir);
-    let testUUID = uuid.v4();
     await trustStore.addRecord(uuid.v4(), 'tset', testUUID, 'data');
     let tsettings = await trustStore.getRecords(testUUID);
     chai.assert(tsettings.length > 0);
@@ -46,8 +44,6 @@ describe('when using Certificate class', () => {
   });
 
   it('can update a record in the TrustStore tsettings', async () => {
-    let trustStore = new TrustStore(assetsDir);
-    let testUUID = uuid.v4();
     await trustStore.addRecord(uuid.v4(), 'tset', testUUID, 'data1');
     let tsettings = await trustStore.getRecords(testUUID);
     chai.assert.equal(tsettings[0].data, 'data1');
@@ -55,35 +51,43 @@ describe('when using Certificate class', () => {
     tsettings = await trustStore.getRecords(testUUID) ;
     chai.assert.equal(tsettings[0].data, 'data2');  
   });
+});
+
+describe('when using Certificate class', () => { 
+
+  before(() => {
+    keychainsDirOriginal = `${assetsDir}/Library/Keychains-Original`;
+    fse.emptyDirSync(keychainsDir);
+    fse.copySync(keychainsDirOriginal, keychainsDir); 
+  });
+
+  beforeEach(async () => {
+    certificate = await new Certificate(`${assetsDir}/test-pem.pem`);
+  });
 
   it('can translate PEM certificate to DER format', async () => {
-    let derData = await new Certificate(`${assetsDir}/test-pem.pem`).getDerData();
+    let derData = await certificate.getDerData();
     let testData = fse.readFileSync(`${assetsDir}/Library/certificates/test-data.txt`);
     chai.assert(testData.equals(derData), 'not translating PEM to DER correctly'); 
   });
  
   it('can get a fingerprint from a PEM certificate', async () => {
-    let certificate = new Certificate(`${assetsDir}/test-pem.pem`);
     let fingerprint = await certificate.getFingerPrint();
     let testFingerprint = fse.readFileSync(`${assetsDir}/Library/certificates/test-fingerprint.txt`);
     chai.assert(fingerprint.equals(testFingerprint));   
   });
 
-  /*it('can get a subject from a PEM certificate', async () => {
-    let subject = await Certificate.getSubject(`${assetsDir}/test-pem.pem`);
-    let testSubject = fse.readFileSync(`${assetsDir}/Library/certificates/test-subj.txt`, 'utf-8');
-    chai.assert.equal(subject, testSubject);
-  });*/
+  it('can get a subject from a PEM certificate', async () => {
+    await certificate.getSubject(`${assetsDir}/test-pem.pem`);
+  });
 
-  it('can add a certificate to a sqlite store', async () => {  
-    let certificate = new Certificate(`${assetsDir}/test-pem.pem`); 
+  it('can add a certificate to a sqlite store', async () => { 
     await certificate.add(assetsDir);
     let hasCert = await certificate.has(assetsDir);
     chai.assert(hasCert, 'after adding a cert, should have one cert in records');
   });  
 
   it('can add and remove a certificate to a sqlite store', async () => {
-    let certificate = new Certificate(`${assetsDir}/test-pem.pem`); 
     await certificate.add(assetsDir);
     let hasCert = await certificate.has(assetsDir);
     chai.assert(hasCert, 'after adding a cert, should have one cert in records');
