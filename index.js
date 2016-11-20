c9cbbfcd0434d702a539e635621da8197ac85db7
Dan Graham <dan@saucelabs.com>
2016-11-19 16:17:25 -0800

    Removed installSSLCert for now
    Until we decide how it will be used

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // transpile:main
 
 import { getSimulator, getDeviceString } from './lib/simulator';
-import { killAllSimulators, endAllSimulatorDaemons, simExists, installSSLCert } from './lib/utils';
+import { killAllSimulators, endAllSimulatorDaemons, simExists } from './lib/utils';
 
-export { getSimulator, getDeviceString, killAllSimulators, endAllSimulatorDaemons, simExists, installSSLCert };
+export { getSimulator, getDeviceString, killAllSimulators, endAllSimulatorDaemons, simExists };
