c25dc692ad5245e0f016e2109f027c8d9d76b8f7
Dan Graham <dan@saucelabs.com>
2016-11-22 14:31:28 -0800

    Lint/hint errors
    Fixed server.js to use async and fixed linting errors. Fixed certificate.js to use 'await' in addRecord

--- test/assets/certificate-test-server/server.js
+++ test/assets/certificate-test-server/server.js
@@ -1,100 +1,105 @@
-/* eslint-disable */
-/* jshint ignore:start */
+/* eslint-disable no-console */
 import https from 'https';
-import pem from 'pem';
-import fs from 'fs';
 import inquirer from 'inquirer';
-import colors from 'colors';
+
 import { installSSLCert, uninstallSSLCert } from '../../../lib/utils';
 import { getDevices } from 'node-simctl';
-import _ from 'lodash';
+import B from 'bluebird';
 
-// Create an HTTPS server with a randomly generated certificate
-pem.createPrivateKey(function (err, key){ 
-  pem.createCertificate({days:1, selfSigned: true, serviceKey: key.key},  async (err, keys) => {
+/* jshint ignore:start */ /* eslint-disable no-unused-vars */
+import colors from 'colors';
+/* eslint-enable */ /* jshint ignore:end */
 
-    // Save that certificate to a file so it can be used later
-    let server = https.createServer({key: keys.serviceKey, cert: keys.certificate}, function (req, res){
-      res.end('If you are seeing this the certificate has been installed');
-    }).listen(9758);
+const pem = B.promisifyAll(require('pem'));
 
-    console.log('Make sure you have at least one IOS Simulator running'.yellow);
-    let devices = await getDevices();
+(async () => {
 
-    // Get currently booted devices
-    let bootedDevices = [];
-    _.forEach(devices, (os) => {
-      os.forEach((device) => {
-        if(device.state === 'Booted'){
-          bootedDevices.push(device);
-        }
-      });
-    });
+  // Create an HTTPS server with a randomly generated certificate
+  let key = await pem.createPrivateKeyAsync();
+  let keys = await pem.createCertificateAsync({days:1, selfSigned: true, serviceKey: key.key});
 
-    if(bootedDevices.length === 0) {
-      return console.log('You must have at least one IOS Simulator running to do this test'.red);
+  let server = https.createServer({key: keys.serviceKey, cert: keys.certificate}, function (req, res) {
+    res.end('If you are seeing this the certificate has been installed');
+  }).listen(9758);
+
+  console.log('Make sure you have at least one IOS Simulator running'.yellow);
+  let devices = await getDevices();
+
+  // Get currently booted devices
+  let bootedDevices = [];
+  for (let osName in devices) {
+    let os = devices[osName];
+    for (let deviceName in os) {
+      let device = os[deviceName];
+      if (device.state === 'Booted') {
+        bootedDevices.push(device);
+      }
     }
+  }
 
-    // Get info for first device
-    let bootedDevice = bootedDevices[0];
-    let udid = bootedDevice.udid;
-    let deviceName = bootedDevice.name;
+  if (bootedDevices.length === 0) {
+    return console.log('You must have at least one IOS Simulator running to do this test'.red);
+  }
 
-    console.log('HTTPS server is running at localhost:9758 and has created a new certificate at "random-pem.pem"'.yellow);  
-    console.log(`Navigate to https://localhost:9758 in '${deviceName} Simulator'`.yellow);
-    console.log('DO NOT PUSH THE CONTINUE BUTTON. PUSH CANCEL.'.red);
-    
-    // Call this if the user answers 'No' to any prompts
-    async function done(){
-      await uninstallSSLCert(keys.certificate, udid);
-      server.close();
-      console.log('Incomplete/failed test'.red);
-    }
+  // Get info for first device
+  let bootedDevice = bootedDevices[0];
+  let udid = bootedDevice.udid;
+  let deviceName = bootedDevice.name;
 
-    let result = await inquirer.prompt([{
-      type: 'confirm',
-      name: 'confirmOpenSite',
-      message: `Is https//localhost:9758 on '${deviceName} Simulator' unaccessible?`,
-    }]);
+  console.log('HTTPS server is running at localhost:9758 and has created a new certificate at "random-pem.pem"'.yellow);  
+  console.log(`Navigate to https://localhost:9758 in '${deviceName} Simulator'`.yellow);
+  console.log('DO NOT PUSH THE CONTINUE BUTTON. PUSH CANCEL.'.red);
 
-    await installSSLCert(keys.certificate, udid);
-
-    if (!result.confirmOpenSite) return done();
-
-    // Apply certificate to Simulator
-    let dir = `${process.env.HOME}/Library/Developer/CoreSimulator/Devices/${udid}/data/`;
-    console.log('Installing certificate'.yellow);
-    console.log(`Certificate installed to '${deviceName} ${udid}'. Navigate back to https://localhost:9758.`.yellow);
-
-    result = await inquirer.prompt([{
-      type: 'confirm',
-      name: 'confirmOpenedSite',
-      message: 'Now is https://localhost:9758 accessible?',
-    }]);
-
-    if(!result.confirmOpenedSite){
-      return done();
-    }
-    
-    // Uninstall cert
-    console.log(`Uninstalling SSL cert`.yellow);
+  // Call this if the user answers 'No' to any prompts
+  async function done () {
     await uninstallSSLCert(keys.certificate, udid);
-    console.log(`SSL cert removed.`.yellow);
-    console.log(`Close the simulator, re-open it and then navigate back to https://localhost:9758`.yellow);
+    server.close();
+    console.log('Incomplete/failed test'.red);
+  }
 
-    result = await inquirer.prompt([{
-      type: 'confirm',
-      name: 'confirmUninstallCert',
-      message: `Is https://localhost:9758 unaccessible?`,
-    }]);
+  let result = await inquirer.prompt([{
+    type: 'confirm',
+    name: 'confirmOpenSite',
+    message: `Is https//localhost:9758 on '${deviceName} Simulator' unaccessible?`,
+  }]);
 
-    if(result.confirmUninstallCert){
-      console.log('Test passed'.green);
-    }
+  console.log('Certificate', keys.certificate, udid);
 
-    return server.close();
-  });
-});
+  await installSSLCert(keys.certificate, udid);
 
-/* jshint ignore:end */
+  if (!result.confirmOpenSite) return done();
+
+  // Apply certificate to Simulator
+  console.log('Installing certificate'.yellow);
+  console.log(`Certificate installed to '${deviceName} ${udid}'. Navigate back to https://localhost:9758.`.yellow);
+
+  result = await inquirer.prompt([{
+    type: 'confirm',
+    name: 'confirmOpenedSite',
+    message: 'Now is https://localhost:9758 accessible?',
+  }]);
+
+  if (!result.confirmOpenedSite) {
+    return done();
+  }
+
+  // Uninstall cert
+  console.log(`Uninstalling SSL cert`.yellow);
+  await uninstallSSLCert(keys.certificate, udid);
+  console.log(`SSL cert removed.`.yellow);
+  console.log(`Close the simulator, re-open it and then navigate back to https://localhost:9758`.yellow);
+
+  result = await inquirer.prompt([{
+    type: 'confirm',
+    name: 'confirmUninstallCert',
+    message: `Is https://localhost:9758 unaccessible?`,
+  }]);
+
+  if (result.confirmUninstallCert) {
+    console.log('Test passed'.green);
+  }
+
+  return server.close();
+})();
+
 /* eslint-enable */
