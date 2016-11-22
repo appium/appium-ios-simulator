c15d58ec2b5dd5094f1d59a12f6687dd6503a0e2
Dan Graham <dan@saucelabs.com>
2016-11-21 19:18:20 -0800

    Used idiomatic assertions
    Made use of the expect style assertions. Also es-disabled a block of code in tail-until because it was giving warnings

--- test/unit/certificate-specs.js
+++ test/unit/certificate-specs.js
@@ -8,7 +8,7 @@ import uuid from 'uuid';
 
 chai.should();
 chai.use(chaiAsPromised);
-
+let expect = chai.expect;
 
 let cwd = process.cwd();
 let assetsDir = `${cwd}/test/assets`;
@@ -30,29 +30,29 @@ describe('when using TrustStore class', () => {
 
   it('can add a record to the TrustStore tsettings', async () => {
     let tsettings = await trustStore.getRecords(testUUID);
-    chai.assert(tsettings.length===0);
+    expect(tsettings).to.have.length.of(0);
     await trustStore.addRecord(uuid.v4(), 'tset', testUUID, 'data');
     tsettings = await trustStore.getRecords(testUUID); 
-    chai.assert(tsettings.length > 0);
-    chai.assert.equal(tsettings[0].subj, testUUID);
+    expect(tsettings).to.have.length.above(0);
+    tsettings[0].subj.should.equal(testUUID);
   });
 
   it('can add and remove records to in TrustStore tsettings', async () => {
     await trustStore.addRecord(uuid.v4(), 'tset', testUUID, 'data');
     let tsettings = await trustStore.getRecords(testUUID);
-    chai.assert(tsettings.length > 0);
+    expect(tsettings).to.have.length.above(0);
     await trustStore.removeRecord(testUUID);
     tsettings = await trustStore.getRecords(testUUID);
-    chai.assert(tsettings.length === 0); 
+    expect(tsettings).to.have.length(0); 
   });
 
   it('can update a record in the TrustStore tsettings', async () => {
     await trustStore.addRecord(uuid.v4(), 'tset', testUUID, 'data1');
     let tsettings = await trustStore.getRecords(testUUID);
-    chai.assert.equal(tsettings[0].data, 'data1');
+    expect(tsettings[0].data).to.equal('data1');
     await trustStore.addRecord(uuid.v4(), 'tset', testUUID, 'data2');
-    tsettings = await trustStore.getRecords(testUUID) ;
-    chai.assert.equal(tsettings[0].data, 'data2');  
+    tsettings = await trustStore.getRecords(testUUID);
+    expect(tsettings[0].data).to.equal('data2');  
   });
 });
 
@@ -65,13 +65,13 @@ describe('when using Certificate class', () => {
   it('can translate PEM certificate to DER format', async () => {
     let derData = await certificate.getDerData();
     let testData = fse.readFileSync(`${assetsDir}/Library/certificates/test-data.txt`);
-    chai.assert(testData.equals(derData), 'not translating PEM to DER correctly'); 
+    expect(testData.equals(derData)); 
   });
  
   it('can get a fingerprint from a PEM certificate', async () => {
     let fingerprint = await certificate.getFingerPrint();
     let testFingerprint = fse.readFileSync(`${assetsDir}/Library/certificates/test-fingerprint.txt`);
-    chai.assert(fingerprint.equals(testFingerprint));   
+    expect(fingerprint.equals(testFingerprint));   
   });
 
   it('can get a subject from a PEM certificate', async () => {
@@ -81,26 +81,26 @@ describe('when using Certificate class', () => {
   it('can add a certificate to a sqlite store', async () => { 
     await certificate.add(assetsDir);
     let hasCert = await certificate.has(assetsDir);
-    chai.assert(hasCert, 'after adding a cert, should have one cert in records');
+    expect(hasCert);
   });  
 
   it('can add and remove a certificate to a sqlite store', async () => {
     await certificate.add(assetsDir);
     let hasCert = await certificate.has(assetsDir);
-    chai.assert(hasCert, 'after adding a cert, should have one cert in records');
+    expect(hasCert);
     await certificate.remove(assetsDir);
     hasCert = await certificate.has(assetsDir);
-    chai.assert(!hasCert, 'after removing the cert, should have no certs left in record');
+    expect(!hasCert);
   });
 
   it('can add a certificate and then remove the same certificate later', async () => {
     await certificate.add(assetsDir);
     let hasCert = await certificate.has(assetsDir);
-    chai.assert(hasCert, 'after adding a cert, should have one cert in records');
+    expect(hasCert);
 
     certificate = new Certificate(`${assetsDir}/test-pem.pem`);
     await certificate.remove(assetsDir);
     hasCert = await certificate.has(assetsDir);
-    chai.assert(!hasCert, 'after removing the cert, should have no certs left in record');
+    expect(!hasCert);
   });
 });
