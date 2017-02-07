1156a6c4ff4f136dc4ff1fd21f1ea82f40263369
Mykola Mokhnach <nick+1@wire.com>
2017-02-07 19:40:14 +0100

    fix typo

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 import { getSimulator, getDeviceString } from './lib/simulator';
 import { killAllSimulators, endAllSimulatorDaemons, simExists, installSSLCert, uninstallSSLCert } from './lib/utils';
-import { BOOT_COMPLETED_EVENT } from './lib/simulator-xcode-6.js';
+import { BOOT_COMPLETED_EVENT } from './lib/simulator-xcode-6';
 
 export { getSimulator, getDeviceString, killAllSimulators, endAllSimulatorDaemons, simExists, installSSLCert, uninstallSSLCert,
          BOOT_COMPLETED_EVENT };
