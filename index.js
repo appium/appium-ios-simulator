383fe97fbb6f72e644fcebfd1b42f0cca8c16170
Dan Graham <dan@saucelabs.com>
2017-03-07 11:58:16 -0800

    Add method that enables calendar access
    * Creates an entry in SQLite where service='kTCCServiceCalendar' and sets allowed to 1
    * Wrote tests to make sure the same entry is never made twice, that it overwrites old entries, etc...

--- index.js
+++ index.js
@@ -2,7 +2,8 @@
 
 import { getSimulator, getDeviceString } from './lib/simulator';
 import { killAllSimulators, endAllSimulatorDaemons, simExists, installSSLCert, uninstallSSLCert } from './lib/utils';
+import { enableCalendarAccess } from './lib/calendar';
 import { BOOT_COMPLETED_EVENT } from './lib/simulator-xcode-6';
 
 export { getSimulator, getDeviceString, killAllSimulators, endAllSimulatorDaemons, simExists, installSSLCert, uninstallSSLCert,
-         BOOT_COMPLETED_EVENT };
+         BOOT_COMPLETED_EVENT, enableCalendarAccess };
--- test/unit/calendar-specs.js
+++ test/unit/calendar-specs.js
@@ -2,10 +2,13 @@
 
 import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
-import { getCalendarDB } from '../../lib/calendar';
+import Calendar from '../../lib/calendar';
 import { fs } from 'appium-support';
 import { copySync } from 'fs-extra';
 import { execSQLiteQuery } from '../../lib/utils';
+import sinon from 'sinon';
+
+const { getCalendarDB, enableCalendarAccess } = Calendar;
 
 chai.should();
 chai.use(chaiAsPromised);
@@ -16,7 +19,7 @@ let assetsDir = `${cwd}/test/assets`;
 let tccDir = `${assetsDir}/Library/TCC`;
 let tccDirOriginal = `${assetsDir}/Library/TCC-Original`;
 
-describe.only('Calendar methods', () => {
+describe('Calendar.js', () => {
 
   beforeEach(async () => {
     await fs.rimraf(tccDir);
@@ -45,5 +48,48 @@ describe.only('Calendar methods', () => {
       let count = parseInt(res.stdout.split('=')[1].trim(), 10);
       expect(count).to.equal(0);
     });
+
+  });
+
+  describe('enableCalendarAccess()', () => {
+    let db;
+    let getCount = Calendar.getCalendarRowCount;
+    let bundleID = 'com.fake.bundleid';
+
+    before(async () => {
+      db = await Calendar.getCalendarDB(assetsDir);
+      sinon.stub(Calendar, 'getCalendarDB').returns(db);
+    });
+
+    after(() => {
+      sinon.restore(Calendar, 'getCalendarDB');
+    });
+
+    it('adds an item to the "access" table called "kTCCServiceCalendar"', async () => {
+      await getCount(db, bundleID).should.eventually.equal(0);
+      await enableCalendarAccess(null, 'com.fake.bundleid');
+      await getCount(db, bundleID).should.eventually.equal(1);
+    });
+
+    it('will not fail and will only creates one entry if we call enableCalendarAccess() twice', async () => {
+      await getCount(db, bundleID).should.eventually.equal(0);
+      await enableCalendarAccess(null, bundleID);
+      await getCount(db, bundleID).should.eventually.equal(1);
+      await enableCalendarAccess(null, bundleID);
+      await getCount(db, bundleID).should.eventually.equal(1);
+    });
+
+    it('overwrites any previous entries', async () => {
+      await execSQLiteQuery(db, `INSERT INTO 'access' VALUES ('kTCCServiceCalendar', '${bundleID}', 0, 0, 0, 0, 0);`);
+      await getCount(db, bundleID).should.eventually.equal(1);
+      let out = (await execSQLiteQuery(db, `SELECT allowed FROM 'access' WHERE client='${bundleID}' AND service='kTCCServiceCalendar'`)).stdout;
+      let allowed = parseInt(out.split('=')[1], 10);
+      expect(allowed).to.equal(0);
+      await enableCalendarAccess(null, bundleID);
+      out = (await execSQLiteQuery(db, `SELECT allowed FROM 'access' WHERE client='${bundleID}' AND service='kTCCServiceCalendar'`)).stdout;
+      allowed = parseInt(out.split('=')[1], 10);
+      expect(allowed).to.equal(1);
+    });
+
   });
 });
