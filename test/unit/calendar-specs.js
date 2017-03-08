ec4d2427591ffddb855fef7f2440adf33ed1db98
Dan Graham <dan@saucelabs.com>
2017-03-08 14:08:54 -0800

    Make calendar a member instance
    * Only instantiate it once
    * Pass bundleID every time instead of making it a member variable
    * Also found some defects in the unit test, it was putting the TCC.db in the wrong directory and wasn't properly testing the calendar methods

--- test/unit/calendar-specs.js
+++ test/unit/calendar-specs.js
@@ -24,7 +24,7 @@ describe('Calendar.js', () => {
   beforeEach(async () => {
     await fs.rimraf(tccDir);
     copySync(tccDirOriginal, tccDir);
-    calendar = new Calendar(tccDir, bundleID);
+    calendar = new Calendar(assetsDir);
   });
 
   after(async () => {
@@ -38,12 +38,12 @@ describe('Calendar.js', () => {
       expect(await fs.exists(tccDir)).to.be.false;
       await calendar.getDB(); // Lazily creates the .db
       expect(await fs.exists(tccDir)).to.be.true;
-      (await calendar.getCalendarRowCount()).should.equal(0);
+      (await calendar.getCalendarRowCount(bundleID)).should.equal(0);
     });
 
     it('doesn\'t overwrite DB if one already exists', async () => {
       let db = await calendar.getDB();
-      let res = await execSQLiteQuery(db, `SELECT count(*) FROM access`);
+      let res = await execSQLiteQuery(db, `SELECT count(*) FROM access WHERE service='kTCCServiceCalendar'`);
       let count = parseInt(res.stdout.split('=')[1].trim(), 10);
       expect(count).to.equal(0);
     });
@@ -52,17 +52,17 @@ describe('Calendar.js', () => {
 
   describe('enableCalendarAccess()', () => {
     it('adds an item to the "access" table called "kTCCServiceCalendar"', async () => {
-      await calendar.getCalendarRowCount().should.eventually.equal(0);
-      await calendar.enableCalendarAccess();
-      await calendar.getCalendarRowCount().should.eventually.equal(1);
+      await calendar.getCalendarRowCount(bundleID).should.eventually.equal(0);
+      await calendar.enableCalendarAccess(bundleID);
+      await calendar.getCalendarRowCount(bundleID).should.eventually.equal(1);
     });
 
     it('will not fail and will only creates one entry if we call enableCalendarAccess() twice', async () => {
-      await calendar.getCalendarRowCount().should.eventually.equal(0);
-      await calendar.enableCalendarAccess();
-      await calendar.getCalendarRowCount().should.eventually.equal(1);
-      await calendar.enableCalendarAccess();
-      await calendar.getCalendarRowCount().should.eventually.equal(1);
+      await calendar.getCalendarRowCount(bundleID).should.eventually.equal(0);
+      await calendar.enableCalendarAccess(bundleID);
+      await calendar.getCalendarRowCount(bundleID).should.eventually.equal(1);
+      await calendar.enableCalendarAccess(bundleID);
+      await calendar.getCalendarRowCount(bundleID).should.eventually.equal(1);
     });
 
     it('overwrites any previous entries', async () => {
@@ -70,13 +70,13 @@ describe('Calendar.js', () => {
 
       // Insert a entry into calendar with 'allowed = 0'
       await execSQLiteQuery(db, `INSERT INTO 'access' VALUES ('kTCCServiceCalendar', '${bundleID}', 0, 0, 0, 0, 0);`);
-      await calendar.getCalendarRowCount().should.eventually.equal(1);
+      await calendar.getCalendarRowCount(bundleID).should.eventually.equal(1);
       let out = (await execSQLiteQuery(db, `SELECT allowed FROM 'access' WHERE client='${bundleID}' AND service='kTCCServiceCalendar'`)).stdout;
       let allowed = parseInt(out.split('=')[1], 10);
       allowed.should.equal(0);
 
       // Now enable the calendar access and check that 'allowed = 1'
-      await calendar.enableCalendarAccess();
+      await calendar.enableCalendarAccess(bundleID);
       out = (await execSQLiteQuery(db, `SELECT allowed FROM 'access' WHERE client='${bundleID}' AND service='kTCCServiceCalendar'`)).stdout;
       allowed = parseInt(out.split('=')[1], 10);
       expect(allowed).to.equal(1);
@@ -86,15 +86,15 @@ describe('Calendar.js', () => {
   describe('disableCalendarAccess()', () => {
 
     it('can enable and then disable', async () => {
-      await calendar.getCalendarRowCount().should.eventually.equal(0);
-      await calendar.enableCalendarAccess();
-      await calendar.getCalendarRowCount().should.eventually.equal(1);
-      await calendar.disableCalendarAccess();
-      await calendar.getCalendarRowCount().should.eventually.equal(0);
+      await calendar.hasCalendarAccess(bundleID).should.eventually.be.false;
+      await calendar.enableCalendarAccess(bundleID);
+      await calendar.hasCalendarAccess(bundleID).should.eventually.be.true;
+      await calendar.disableCalendarAccess(bundleID);
+      await calendar.hasCalendarAccess(bundleID).should.eventually.be.false;
     });
 
     it('does nothing if disableCalendarAccess called without calendar access being enabled', async () => {
-      await calendar.disableCalendarAccess().should.not.be.rejected;
+      await calendar.disableCalendarAccess(bundleID).should.not.be.rejected;
     });
 
   });
