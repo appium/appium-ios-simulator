2566c847cdeed0ecf7bbf2bd49de13fe7781ffa1
Isaac Murchie <isaac@saucelabs.com>
2017-08-14 08:08:31 -0400

    Shift to mocha call

--- gulpfile.js
+++ gulpfile.js
@@ -5,11 +5,8 @@ const boilerplate = require('appium-gulp-plugins').boilerplate.use(gulp);
 
 boilerplate({
   build: 'appium-ios-simulator',
-  e2eTest: {
-    files: 'build/test/functional/**/*-e2e-specs.js',
-  },
   coverage: {
     files: ['./test/unit/**/*-specs.js'],
-    verbose: true,
+    verbose: true
   },
 });
