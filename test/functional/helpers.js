db7cdadca3627169aae137fd9cff0f3a00eefd00
Mykola Mokhnach <nick+1@wire.com>
2017-12-09 12:13:54 +0100

    Remove unnecessary delays

--- test/functional/helpers.js
+++ test/functional/helpers.js
@@ -1,14 +1,10 @@
-import { waitForCondition } from 'asyncbox';
-
 const LONG_TIMEOUT = 480 * 1000;
 
 async function verifyStates (sim, shouldServerRun, shouldClientRun) {
   const isServerRunning = await sim.isRunning();
   isServerRunning.should.eql(shouldServerRun);
-  await waitForCondition(async () => {
-    const isClientRunning = await sim.isUIClientRunning();
-    return isClientRunning === shouldClientRun;
-  }, {waitMs: 60000, intervalMs: 5000});
+  const isClientRunning = await sim.isUIClientRunning();
+  isClientRunning.should.eql(shouldClientRun);
 }
 
 export { LONG_TIMEOUT, verifyStates };
