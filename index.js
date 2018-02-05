ece85116f4db278d395880d3f999ac4ba1a3da1b
dan@saucelabs.com <dan@saucelabs.com>
2018-02-05 15:59:48 -0800

    Had 'hasSSLCert' to API
    * Exposed 'hasSSLCert' to API which checks new pemText to check if it already exists as an entry in the Trust Store
    * Updated the certificate.has method to check fingerprint too
      * If the subj is there but fingerprint is different, then it returns false
      * If subj and sha1 are unchanged, then it returns true
      * This is so that we can check if a cert is already installed, and if it is, we don't have to restart simulator

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // transpile:main
 
 import { getSimulator, getDeviceString } from './lib/simulator';
-import { killAllSimulators, endAllSimulatorDaemons, simExists, installSSLCert, uninstallSSLCert } from './lib/utils';
+import { killAllSimulators, endAllSimulatorDaemons, simExists, installSSLCert, uninstallSSLCert, hasSSLCert } from './lib/utils';
 import { BOOT_COMPLETED_EVENT } from './lib/simulator-xcode-6';
 
-export { getSimulator, getDeviceString, killAllSimulators, endAllSimulatorDaemons, simExists, installSSLCert, uninstallSSLCert,
+export { getSimulator, getDeviceString, killAllSimulators, endAllSimulatorDaemons, simExists, installSSLCert, uninstallSSLCert, hasSSLCert,
          BOOT_COMPLETED_EVENT };
