36ac4764eef76ac4b8f44bd1e52910c01c0bb439
dan@saucelabs.com <dan@saucelabs.com>
2018-02-06 13:51:03 -0800

    PR fixes

--- index.js
+++ index.js
@@ -4,5 +4,14 @@ import { getSimulator, getDeviceString } from './lib/simulator';
 import { killAllSimulators, endAllSimulatorDaemons, simExists, installSSLCert, uninstallSSLCert, hasSSLCert } from './lib/utils';
 import { BOOT_COMPLETED_EVENT } from './lib/simulator-xcode-6';
 
-export { getSimulator, getDeviceString, killAllSimulators, endAllSimulatorDaemons, simExists, installSSLCert, uninstallSSLCert, hasSSLCert,
-         BOOT_COMPLETED_EVENT };
+export {
+  getSimulator,
+  getDeviceString,
+  killAllSimulators,
+  endAllSimulatorDaemons,
+  simExists,
+  installSSLCert,
+  uninstallSSLCert,
+  hasSSLCert,
+  BOOT_COMPLETED_EVENT
+};
