7a6d9daf05e2a6b8dc74f147963973bf221440ad
Kazuaki MATSUO <fly.49.89.over@gmail.com>
2018-07-09 00:14:34 +0900

    add tests for getBiometric

---
+++ test/unit/simulator-xcode-9-specs.js
@@ -0,0 +1,36 @@
+// transpile:mocha
+
+import chai from 'chai';
+import chaiAsPromised from 'chai-as-promised';
+import SimulatorXcode9 from "../../lib/simulator-xcode-9";
+
+chai.should();
+chai.use(chaiAsPromised);
+
+const XCODE_VERSION_9 = {
+  versionString: '9.0',
+  versionFloat: 9.0,
+  major: 9,
+  minor: 0,
+  patch: undefined
+};
+
+describe('SimulatorXcode9', function () {
+  let simulatorXcode9;
+
+  beforeEach(function () {
+    simulatorXcode9 = new SimulatorXcode9('1234', XCODE_VERSION_9);
+  });
+
+  describe('getBiometric', function () {
+    it('return touch id object', async function () {
+      simulatorXcode9.getBiometric('touchId').should.eql({ menuName: 'Touch Id' });
+    });
+
+    it('raise an error since the argument does not exist in biometric', async function () {
+      (function () {
+        simulatorXcode9.getBiometric('no-touchId');
+      }).should.throw(Error, 'no-touchId is not a valid biometric. Use one of: ["touchId","faceId"]');
+    });
+  });
+});
