ad160ae22d25f7545f115bf58870e2d22305f5f2
Dan <dpgraham.85@gmail.com>
2020-09-16 16:03:38 -0700

    Use MAX_VALUE for highest Xcode version

--- test/unit/simulator-specs.js
+++ test/unit/simulator-specs.js
@@ -56,7 +56,7 @@ describe('simulator', function () {
       [10, 0, '10.0.0', SimulatorXcode10],
       [11, 0, '11.0.0', SimulatorXcode11],
       [11, 4, '11.4.0', SimulatorXcode11_4],
-      [Infinity, 0, '100000000.0.0', SimulatorXcode11_4],
+      [Number.MAX_VALUE, 0, `${Number.MAX_VALUE}.0.0`, SimulatorXcode11_4],
     ];
 
     for (const [major, minor, versionString, expectedXcodeClass] of xcodeVersions) {
