179bfdec8f2566e92c22414a22aa3d25a7db177b
rerorero <nato9598@hotmail.co.jp>
2022-12-09 21:12:43 +0900

    feat: add setReduceTrancparency

--- test/unit/settings-specs.js
+++ test/unit/settings-specs.js
@@ -309,4 +309,31 @@ describe('settings', function () {
       settings.setReduceMotion.calledOnce.should.eql(true);
     });
   });
+
+  describe('setReduceTransparency', function () {
+    let sim;
+    before(function () {
+      sim = new SimulatorXcode6();
+      sinon.stub(settings, 'setReduceTransparency');
+    });
+
+    afterEach(function () {
+      sinon.restore();
+      sinon.stub(settings, 'setReduceTransparency');
+    });
+
+    it('should launch simulator if fresh before setting reduce transparency', async function () {
+      sinon.stub(sim, 'isFresh').returns(true);
+      sinon.stub(sim, 'launchAndQuit');
+      await sim.setReduceTransparency(true);
+      sim.launchAndQuit.calledOnce.should.eql(true);
+      settings.setReduceTransparency.calledOnce.should.eql(true);
+    });
+
+    it('should just set reduce transparency', async function () {
+      sinon.stub(sim, 'isFresh').returns(false);
+      await sim.setReduceTransparency(true);
+      settings.setReduceTransparency.calledOnce.should.eql(true);
+    });
+  });
 });
