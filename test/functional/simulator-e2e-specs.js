9a3fd7a1863f0e8dee15de420c184e120ce5882a
rerorero <nato9598@hotmail.co.jp>
2023-09-06 21:20:17 +0900

    feat: setAutoFillPasswordsEnabled

--- test/functional/simulator-e2e-specs.js
+++ test/functional/simulator-e2e-specs.js
@@ -322,6 +322,13 @@ describe('advanced features', function () {
     });
   });
 
+  describe(`setAutoFillPasswordsEnabled`, function () {
+    it('should update AutoFill Passwords settings', async function () {
+      await sim.setAutoFillPasswordsEnabled(true);
+      await sim.setAutoFillPasswordsEnabled(false);
+    });
+  });
+
   describe('Safari', function () {
     it('should scrub Safari', async function () {
       if (xcodeVersion.major === 13 && process.env.CI) {
